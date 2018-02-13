8f403ac48ded1c5f03ca1f2ad2232c9f2a98b5d5
cesargamboa <35382861+cesargamboa@users.noreply.github.com>
2018-02-13 01:14:21 -0600

    qq

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,8 @@ import '../css/bootstrap.min.css';
 import '../css/bootstrap.min-lux.css';
 import cesar from '../logos/cesar.png';
 import { Link } from 'react-router-dom';
+import "../residentevil.jpg"; 
+
 
 const header = (props) => {
     return (
